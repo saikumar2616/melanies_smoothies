c24ada801723869fa48b56ca322cfa5c2dbc0588
Sai kumar <sirigirisaikumar0@gmail.com>
2024-05-08 21:54:31 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -58,5 +58,7 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered, '+ name_on_order + " !", icon="✅")
         
-
-         
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+# st.text(fruityvice_response)
+fv_df = st.dataframe(data = fruityvice_response.json(), use_container_width=True)
